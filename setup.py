9346b4fb00c614e45eedc0ef1a36655274e2659b
sun589 <73369511+sun589@users.noreply.github.com>
2024-09-22 20:27:22 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyQQSkeyTool',
+    version='1.0.0',
+    author='sun589',
+    author_email='goodluck1787@outlook.com',
+    description='一款集成了QQSkey/QQClientkey获取的库',
+    url='https://github.com/sun589/PyQQSkeyTool',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
