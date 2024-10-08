9eb9d0a15ceff73183f1f2619ac7cc42ab4c3efd
sun589 <73369511+sun589@users.noreply.github.com>
2024-10-08 20:36:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,4 +13,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
+    install_requires=[
+        'requests>=2.25.1',
+        'urlextract>=1.6.0'
+    ],
+    python_requires='>=3.6'
 )
